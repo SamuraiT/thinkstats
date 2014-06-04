3253b3c7cad42dc351bfeee1a6ca205d17daf91d
SamuraiT <t.yasukawa01@gmail.com>
2014-06-04 09:37:45 +0900

    modify a bit

--- chap4/pareto.py
+++ chap4/pareto.py
@@ -12,9 +12,11 @@ import myplot
 
 def paretovariate(alpha, xm=1, n=1000):
     pareto = random.paretovariate
-    y = lambda alpha,xm:((1/xm)**alpha)*pareto(alpha)+(1-(1/xm))
+    y = lambda alpha,xm:((1.0/xm)**alpha)*pareto(alpha)+(1-(1.0/xm))
     return [y(alpha,xm) for i in xrange(n)]
 
+def ccdf_list(cdf):
+    return [1-p for x,p in cdf.Items()]
 
 def main():
     pareto = paretovariate(1,0.5)
@@ -22,7 +24,7 @@ def main():
     myplot.Cdf(cdf)
     myplot.show()
 
-    ccdf = [1-p for x,p in cdf.Items()]
+    ccdf = ccdf_list(cdf)
     plt = myplot.pyplot
     plt.plot(cdf.Values(), ccdf)
     plt.xscale('log')
